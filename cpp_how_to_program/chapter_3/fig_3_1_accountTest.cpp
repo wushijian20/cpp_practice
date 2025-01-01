fea9c79c15d68f7ebaf27dd5b80f4c1c7192356f
wushijian20 <wushijian20@gmail.com>
2025-01-01 09:13:23 -0500

    fig_3_1_accountTest

---
+++ cpp_how_to_program/chapter_3/fig_3_1_accountTest.cpp
@@ -0,0 +1,10 @@
+// Fig. 3.1: AccountTest.cpp
+// Creating and manipulating an Account object.
+#include <iostream>
+#include <string>
+#include "Account.h"
+using namespace std;
+
+int main(){
+    Account myAccount; 
+}
